cf47184a55c3cd5e3581d0be52f6b2166d1af517
Pralvin <12032568@mail.sustech.edu.cn>
2021-08-01 16:13:36 +0800

    python实战2提交代码

--- hello.py
+++ hello.py
@@ -1,4 +1,28 @@
-print ("hello,world")
-print("add new content")
-print("add 2")
-123
+class animal:
+    def __init__(self,name,color,age,gender):
+        self.name = name
+        self.color = color
+        self.age = age
+        self.gender = gender
+    def call(self,voice):
+        print("voice")
+    def run(self):
+        print("can run")
+
+class cat(animal):
+    def __init__(self,name,color,age,gender,):
+        self.hair ="short hair"
+        super().__init__(name,color,age,gender)
+    def catch_rat(self):
+        print("can catch rat")
+    def call(self,voice):
+        print("喵喵叫")
+
+class dog(animal):
+    def __init__(self,name,color,age,gender,):
+        self.hair ="long hair"
+        super().__init__(name,color,age,gender)
+    def caretaker(selfL):
+        print("can watch home")
+    def call(self,voice):
+        print("汪汪叫")
